ebc5958f9a1c11ef23a970affc2646e6940c8521
Smilie Pop <arisatominato48@gmail.com>
2025-04-18 19:18:14 +0800

    Add background on content

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,11 +1,26 @@
-import React from "react";
+import React, { useContext, useEffect, useRef } from "react";
 import Canvas from "./Canvas";
 import SidebarSelect from "./SidebarSelect";
 import DownloadClearButtons from "./DownloadClearButtons";
+import { AppContext } from "../contexts/AppContext";
 
 const Content: React.FC = () => {
+    const contentBackground = useRef<HTMLDivElement | null>(null);
+    const context = useContext(AppContext);
+
+    useEffect(() => {
+        if (!context || !context.background) return;
+        if (contentBackground.current) {
+            contentBackground.current.style.backgroundImage = context
+                ?.background.filename
+                ? `url(${context.background.filename})`
+                : "";
+        }
+    }, [context]);
+
     return (
         <div id="content" className="center" style={{ position: "relative" }}>
+            <div id="content-background" ref={contentBackground}></div>
             <SidebarSelect />
             <DownloadClearButtons />
             <Canvas />
