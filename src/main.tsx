0e9dbf79d9edd2972f692c860b875f224d61d7fe
Smilie Pop <arisatominato48@gmail.com>
2025-04-20 14:44:02 +0800

    Added Vercel Analytics

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,14 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import { AppProvider } from "./contexts/AppProvider.tsx";
-import "./css/main.css"
+import { Analytics } from "@vercel/analytics/react";
+import "./css/main.css";
 
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
         <AppProvider>
             <App />
+            <Analytics />
         </AppProvider>
     </StrictMode>
 );
