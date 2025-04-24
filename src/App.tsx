b0910ab18696ee1c8915ab4e938d11a78f86e921
Smilie Pop <arisatominato48@gmail.com>
2025-04-24 09:05:16 +0800

    Removed announcement

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,20 @@ import { useContext } from "react";
 import Content from "./components/Content";
 import Sidebar from "./components/Sidebar";
 import { AppContext } from "./contexts/AppContext";
-import Announcements from "./components/Announcements";
+// import Announcements from "./components/Announcements";
 
 function App() {
     const context = useContext(AppContext);
     if (!context) {
         throw new Error("Context is not loaded properly.");
     }
-    const { hide, hideAnnouncements } = context;
+    const { hide } = context;
 
     return (
         <main>
             <Content />
             {!hide && <Sidebar />}
-            {!hideAnnouncements && <Announcements />}
+            {/* {!hideAnnouncements && <Announcements />} */}
         </main>
     );
 }
