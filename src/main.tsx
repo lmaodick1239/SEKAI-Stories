e5b0897009aeda24a1b44c59543eb4efa5841382
Smilie Pop <arisatominato48@gmail.com>
2025-04-26 14:49:16 +0800

    Removed Vercel Analytics

--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,12 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import { AppProvider } from "./contexts/AppProvider.tsx";
-import { Analytics } from "@vercel/analytics/react";
 import "./css/main.css";
 
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
         <AppProvider>
             <App />
-            <Analytics />
         </AppProvider>
     </StrictMode>
 );
