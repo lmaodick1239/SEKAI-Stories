0bb151eb0d79b6d32099bbee1d8393fa70b517a3
Smilie Pop <arisatominato48@gmail.com>
2025-10-18 22:34:26 +0800

    Use the Tutorial's Y-Offset initial setup on Text Menu

--- src/components/Sidebar/Options/Text/YOffset.tsx
+++ src/components/Sidebar/Options/Text/YOffset.tsx
@@ -1,47 +1,24 @@
-import React, { useContext } from "react";
+import React, { useState } from "react";
 import { useTranslation } from "react-i18next";
-import { SceneContext } from "../../../../contexts/SceneContext";
+import AdjustYPosition from "../../../AdjustYPosition";
 
 const YOffset: React.FC = () => {
     const { t } = useTranslation();
 
-    const scene = useContext(SceneContext);
-    if (!scene) throw new Error("Context not loaded");
-
-    const { text, setText } = scene;
-
-    if (!text) return <p>{t("please-wait")}</p>;
-
-    const handleYOffset = (event: React.ChangeEvent<HTMLInputElement>) => {
-        const value = Number(event.target.value);
-        localStorage.setItem("textAlignment", String(value));
-        text.nameTag.y = 780 + value;
-        text.dialogue.y = 845 + value;
-        setText({
-            ...text,
-            yOffset: value,
-        });
-    };
-
+    const [showAdjust, setShowAdjust] = useState<boolean>(false);
     return (
         <>
-            <h3> Adjustment: ({text.yOffset}px)</h3>
-            <input
-                type="range"
-                name="offset"
-                id="offset"
-                min={-20}
-                max={20}
-                value={text.yOffset}
-                step={1}
-                onChange={handleYOffset}
-            />
-            <p>{t("text.y-offset-details")}</p>
-            <img
-                src="/img/y-offset.png"
-                alt="y-offset"
-                className="width-100 margin-top-10"
-            />
+
+            <button
+                className="btn-regular btn-100 btn-blue"
+                onClick={() => {
+                    setShowAdjust(true);
+                }}
+                >
+                {t("adjust")}
+            </button>
+            {showAdjust && <AdjustYPosition show={setShowAdjust} />}
+                <p>{t("text.y-offset-details")}</p>
         </>
     );
 };
